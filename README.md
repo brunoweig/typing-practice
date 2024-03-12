9b2515d66ffd1d20ae9a05cf7fe7562d18f9742a
Bruno Weigmann de Matos <mouts.bruno@tigre.com>
2024-03-12 18:22:48 -0300

    setup inicial

---
+++ README.md
@@ -0,0 +1,3 @@
+Typing Practice
+
+https://github.com/florinpop17/app-ideas/blob/master/Projects/2-Intermediate/Typing-Practice-App.md
---
+++ jest.config.js
@@ -0,0 +1,32 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+/** @type {import('jest').Config} */
+const config = {
+  // Indicates whether the coverage information should be collected while executing the test
+  collectCoverage: false,
+
+  // An array of glob patterns indicating a set of files for which coverage information should be collected
+  // collectCoverageFrom: undefined,
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: "coverage",
+
+  // An array of regexp pattern strings used to skip coverage collection
+  // coveragePathIgnorePatterns: [
+  //   "/node_modules/"
+  // ],
+
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: "v8",
+
+  // A map from regular expressions to paths to transformers
+  transform: {},
+
+  // Indicates whether each individual test should be reported during the run
+  // verbose: undefined,
+};
+
+export default config;
